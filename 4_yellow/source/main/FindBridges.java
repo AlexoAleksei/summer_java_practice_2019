3e7893b63b58af0115c22c2564ca9d243faf8e46
AlexoAleksei <31552198+AlexoAleksei@users.noreply.github.com>
2019-07-08 20:31:53 +0300

    Add files via upload

---
+++ 4_yellow/source/main/FindBridges.java
@@ -0,0 +1,11 @@
+import gui.Window;
+
+import javax.swing.*;
+
+public class FindBridges {
+    public FindBridges() { /* compiled code */ }
+
+    public static void main(java.lang.String[] args) {
+        JFrame myWindow = new Window();
+         }
+}
