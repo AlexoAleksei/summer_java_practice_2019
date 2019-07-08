df443c937994c9ea6a76a6ce8396b9b5fde288fe
AlexoAleksei <31552198+AlexoAleksei@users.noreply.github.com>
2019-07-08 19:55:28 +0300

    Delete GraphDrawer.java

--- 4_yellow/Project_files/src/draw/GraphDrawer.java
+++
@@ -1,8 +0,0 @@
-package src.draw;
-
-import java.awt.*;
-
-public class GraphDrawer {
-    private Point[] vertexCenters;
-    private Point[] edgeCoords; //ассцоиативный массив с шагом 2 (начало - конец ребра)
-}
