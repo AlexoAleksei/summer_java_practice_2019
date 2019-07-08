1a34b1e1256d0e31caadf425f4c7d74f9603d1b1
AlexoAleksei <31552198+AlexoAleksei@users.noreply.github.com>
2019-07-08 20:06:27 +0300

    Add files via upload

---
+++ 4_yellow/source/main/draw/EdgeDrawer.java
@@ -0,0 +1,13 @@
+package draw;
+
+import java.awt.Graphics;
+import java.awt.Point;
+import javax.swing.JPanel;
+
+public class EdgeDrawer extends JPanel {
+    public EdgeDrawer(Point one, Point two) {
+    }
+
+    public void paint(Graphics g) {
+    }
+}
---
+++ 4_yellow/source/main/draw/VertexDrawer.java
@@ -0,0 +1,13 @@
+package draw;
+
+import java.awt.Graphics;
+import java.awt.Point;
+import javax.swing.JPanel;
+
+public class VertexDrawer extends JPanel {
+    public VertexDrawer(Point one) {
+    }
+
+    public void paint(Graphics g) {
+    }
+}
